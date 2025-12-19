f5fbd4eb5e89ccbad68ec0939204d27843dcf421
acalderonq-source <acalderon.q@tomza.com>
2025-12-19 11:27:48 -0600

    WW

--- src/server.js
+++ src/server.js
@@ -29,4 +29,8 @@ app.use("/unidades", unidadesRoutes);
 app.use("/agenda", agendaRoutes);
 app.use("/mantenimientos", mantenimientosRoutes);
 
-app.listen(process.env.PORT||3000, ()=>console.log("Servidor listo"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto", PORT);
+});
