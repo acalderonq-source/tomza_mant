6dc8ce59cf81acf8484de0f8acd83a0e676064ad
acalderonq-source <acalderon.q@tomza.com>
2026-05-21 11:54:57 -0600

    S

--- src/utils/sedes.js
+++ src/utils/sedes.js
@@ -3,19 +3,49 @@ function getSedesPermitidas(req) {
   let sedes = [];
 
   if (user.rol === "ADMIN") {
-    if (req.session.sedeSeleccionada && req.session.sedeSeleccionada !== "TODAS") {
+
+    if (
+      req.session.sedeSeleccionada &&
+      req.session.sedeSeleccionada !== "TODAS"
+    ) {
+
       sedes = [req.session.sedeSeleccionada];
+
     } else {
-      sedes = ["Cartago", "Guapiles","La Cruz", "Transportadora", "Granel", "Alajuela"]; // todas las sedes disponibles
+
+      sedes = [
+        "Cartago",
+        "Guapiles",
+        "La Cruz",
+        "Transportadora",
+        "Granel",
+        "Alajuela",
+        "Tecnicos",
+        "Taller",
+        "San Carlos",
+        "Rio Claro",
+        "Perez Zeledon",
+        "Nicoya"
+      ];
+
     }
+
   } else if (user.usuario === "pesados") {
+
     if (req.session.sedeSeleccionada) {
+
       sedes = [req.session.sedeSeleccionada];
+
     } else {
-      sedes = ["Transportadora"]; // por defecto
+
+      sedes = ["Transportadora"];
+
     }
+
   } else {
+
     sedes = [user.sede];
+
   }
 
   return sedes;
